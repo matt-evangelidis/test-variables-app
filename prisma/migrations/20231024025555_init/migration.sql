83b2e7660e96a4d3f4b10e7d600c59bf034e0e51
Thomas Clark <thomas.clark@bigtincan.com>
2023-10-24 14:03:23 +1100

    changed post model

--- prisma/migrations/20231024025555_init/migration.sql
+++ prisma/migrations/20231024025555_init/migration.sql
@@ -1,10 +1,14 @@
 -- CreateTable
 CREATE TABLE "Post" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateIndex
-CREATE INDEX "Post_name_idx" ON "Post"("name");
+CREATE INDEX "Post_title_idx" ON "Post"("title");
+
+-- CreateIndex
+CREATE INDEX "Post_content_idx" ON "Post"("content");
--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -7,12 +7,12 @@ import { api } from "~/trpc/react";
 
 export function CreatePost() {
   const router = useRouter();
-  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
 
   const createPost = api.post.create.useMutation({
     onSuccess: () => {
       router.refresh();
-      setName("");
+      setTitle("");
     },
   });
 
@@ -20,16 +20,16 @@ export function CreatePost() {
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        createPost.mutate({ name });
+        createPost.mutate({ title });
       }}
       className="flex flex-col gap-2"
     >
       <input
         type="text"
         placeholder="Title"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        className="w-full rounded-full px-4 py-2 text-black"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="rounded-full w-full px-4 py-2 text-black"
       />
       <button
         type="submit"
